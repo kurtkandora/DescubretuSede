7927bb261b8ac9e7877e360d4f4b2c8b34d8f3a5
cmaturana <carloshp93@gmail.com>
2014-10-16 17:25:09 -0300

    modelo actualizado
    por ibarra

--- Web/descubretusede/model/Notificacion.php
+++ Web/descubretusede/model/Notificacion.php
@@ -2,15 +2,19 @@
    class Notificacion {
  	public $id_notificacion;
 	public $id_actividad;
+	public $fecha_notificacion;
+	public $descrip_notifi;
 	
 	function __construct()
 	{
 	}
  	
-	function init($id_notificacion,$id_actividad)
+	function init($id_notificacion,$id_actividadpublic,$fecha_notificacion,$descrip_notifi)
 	{
 		$this->id_notificacion = $id_notificacion;
 		$this->id_actividad=$id_actividad;
+		$this->fecha_notificacion=$fecha_notificacion;
+		$this->descrip_notifi=$descrip_notifi;
 		
 	}
 	
